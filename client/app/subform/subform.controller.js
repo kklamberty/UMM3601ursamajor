7849fdf72b589858079cb982432e961a639edff2
Jacoby04 <opdah023@morris.umn.edu>
2014-10-09 13:50:52 -0500

    made some progress on the form, still having issues with labels and stuff.

--- client/app/subform/subform.controller.js
+++ client/app/subform/subform.controller.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('umm3601ursamajorApp')
-  .controller('SubformCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('SubformCtrl', function ($scope, Auth) {
+    $scope.isLoggedIn = Auth.isLoggedIn;
+
+    $scope.formatOptions =
+        ['Artist Statement',
+         'Humanities Proposal',
+         'Science or Social Science Abstract'
+        ];
+
+    $scope.presentationTypes =
+        ['Poster or visual display',
+         'Oral presentation',
+         'Performance'
+        ];
+
+    $scope.values = [
+        {pres_title: String},
+        {pres_abstract: String}
+    ];
+
+    $scope.charsRemaining = function() {
+        return 1000 - $scope.values.pres_abstract.length;
+    }
+
   });
