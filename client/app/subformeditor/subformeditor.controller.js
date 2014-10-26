6ad6c6a44932c66c230ae973eb644e07aa44c3b5
Jacoby04 <opdah023@morris.umn.edu>
2014-10-26 15:51:38 -0500

    Updated the schema for subformtext.

--- client/app/subformeditor/subformeditor.controller.js
+++ client/app/subformeditor/subformeditor.controller.js
@@ -3,11 +3,11 @@
 angular.module('umm3601ursamajorApp')
   .controller('SubformeditorCtrl', function ($scope) {
 
-        $scope.submissionText = {
+        $scope.submissionText = {};
 
-
-
-        };
+        // Need to get what should be the only seed subformtext from the database.
+        // that will be the one that is updated each time the editor is used.
+        //$http.get('/api/subformtext').success(function(submissionText))
 
 
 
