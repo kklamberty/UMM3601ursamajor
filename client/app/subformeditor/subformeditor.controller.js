d0790b874e17503e647e97c47a1596289a9d800b
Jacoby04 <opdah023@morris.umn.edu>
2014-10-27 18:29:44 -0500

    Working on storing changes in the database for the subform editor.

--- client/app/subformeditor/subformeditor.controller.js
+++ client/app/subformeditor/subformeditor.controller.js
@@ -2,9 +2,11 @@
 
 angular.module('umm3601ursamajorApp')
   .controller('SubformeditorCtrl', function ($scope, $http, Auth, $location) {
+
         if(Auth.isLoggedIn() === false) {
             $location.path('/');
         }
+
         $scope.submissionTextArray = [];
         $scope.submissionText = {};
 
@@ -15,6 +17,85 @@ angular.module('umm3601ursamajorApp')
             $scope.submissionText = $scope.submissionTextArray[0];
         });
 
+        $scope.submitChanges = function() {
 
+//            $http.put('api/subformtexts/:$scope.submissionText._id' ,
+//                {
+//                    title: $scope.submissionText.title,
+//                    adviserDisclaimer: $scope.submissionText.adviserDisclaimer,
+//                    ursSummary: $scope.submissionText.ursSummary,
+//                    notes1: $scope.submissionText.notes1,
+//                    nameDisclaimer: $scope.submissionText.nameDisclaimer,
+//                    header1: $scope.submissionText.header1,
+//                    criteria: $scope.submissionText.criteria,
+//                    header2: $scope.submissionText.header2,
+//                    notes2: $scope.submissionText.notes2,
+//                    artistCriteria: $scope.submissionText.artistCriteria,
+//                    humanitiesCriteria: $scope.submissionText.humanitiesCriteria,
+//                    scienceCriteria: $scope.submissionText.scienceCriteria,
+//                    notes3: $scope.submissionText.notes3,
+//                    submissionTitle: $scope.submissionText.submissionTitle,
+//                    submissionFormat: $scope.submissionText.Format,
+//                    submissionAbstract: $scope.submissionText.submissionAbstract,
+//                    submissionAbstractNotes: $scope.submissionText.submissionAbstractNotes,
+//                    submissionPresentationType: $scope.submissionText.submissionPresentationType,
+//                    submissionFormatChange: $scope.submissionText.submissionFormatChange,
+//                    submissionChangeNotes: $scope.submissionText.submissionChangeNotes,
+//                    submissionPresenter: $scope.submissionText.submissionPresenter,
+//                    submissionCopresenterOne: $scope.submissionText.submissionCopresenterOne,
+//                    submissionCopresenterTwo: $scope.submissionText.submissionCopresenterTwo,
+//                    submissionSponsors: $scope.submissionText.submissionSponsors,
+//                    submissionSponsorsNotes: $scope.submissionText.submissionSponsorsNotes,
+//                    submissionAdviser: $scope.submissionText.submissionAdviser,
+//                    submissionAdviserNotes: $scope.submissionText.submissionAdviserNotes,
+//                    submissionFeatured: $scope.submissionText.submissionFeatured,
+//                    submissionMediaServices: $scope.submissionText.submissionMediaServices,
+//                    submissionSpecialRequirements: $scope.submissionText.submissionSpecialRequirements,
+//                    submissionTee: $scope.submissionText.submissionTee,
+//                    submissionTeeNotes: $scope.submissionText.submissionTeeNotes,
+//                    submissionOther: $scope.submissionText.submissionOther
+//                }
+//            );
+
+            $http.post('api/subformtexts/' ,
+                {
+                    title: $scope.submissionText.title,
+                    adviserDisclaimer: $scope.submissionText.adviserDisclaimer,
+                    ursSummary: $scope.submissionText.ursSummary,
+                    notes1: $scope.submissionText.notes1,
+                    nameDisclaimer: $scope.submissionText.nameDisclaimer,
+                    header1: $scope.submissionText.header1,
+                    criteria: $scope.submissionText.criteria,
+                    header2: $scope.submissionText.header2,
+                    notes2: $scope.submissionText.notes2,
+                    artistCriteria: $scope.submissionText.artistCriteria,
+                    humanitiesCriteria: $scope.submissionText.humanitiesCriteria,
+                    scienceCriteria: $scope.submissionText.scienceCriteria,
+                    notes3: $scope.submissionText.notes3,
+                    submissionTitle: $scope.submissionText.submissionTitle,
+                    submissionFormat: $scope.submissionText.Format,
+                    submissionAbstract: $scope.submissionText.submissionAbstract,
+                    submissionAbstractNotes: $scope.submissionText.submissionAbstractNotes,
+                    submissionPresentationType: $scope.submissionText.submissionPresentationType,
+                    submissionFormatChange: $scope.submissionText.submissionFormatChange,
+                    submissionChangeNotes: $scope.submissionText.submissionChangeNotes,
+                    submissionPresenter: $scope.submissionText.submissionPresenter,
+                    submissionCopresenterOne: $scope.submissionText.submissionCopresenterOne,
+                    submissionCopresenterTwo: $scope.submissionText.submissionCopresenterTwo,
+                    submissionSponsors: $scope.submissionText.submissionSponsors,
+                    submissionSponsorsNotes: $scope.submissionText.submissionSponsorsNotes,
+                    submissionAdviser: $scope.submissionText.submissionAdviser,
+                    submissionAdviserNotes: $scope.submissionText.submissionAdviserNotes,
+                    submissionFeatured: $scope.submissionText.submissionFeatured,
+                    submissionMediaServices: $scope.submissionText.submissionMediaServices,
+                    submissionSpecialRequirements: $scope.submissionText.submissionSpecialRequirements,
+                    submissionTee: $scope.submissionText.submissionTee,
+                    submissionTeeNotes: $scope.submissionText.submissionTeeNotes,
+                    submissionOther: $scope.submissionText.submissionOther
+                }
+            );
+
+            $location.path('/admin');
+        };
 
   });
