7b4f00e28f0f1c40cbf5ca7ba20977a255714b86
casal033 <casal033@morris.umn.edu>
2014-11-14 11:51:42 -0600

    Fixed tests

--- client/components/sublist/sublist.controller.spec.js
+++ client/components/sublist/sublist.controller.spec.js
@@ -8,14 +8,14 @@ describe('filter', function() {
 
     beforeEach(module('umm3601ursamajorApp'));
 
-    describe('isntempty', function() {
+    describe('isntEmpty', function() {
 
         it('should return return the title and item ' +
             'if the item is an object with length over 0, empty string or null',
-          inject(function(isntemptyFilter) {
-              expect(isntemptyFilter("item1", "First Item")).toBe("First Item item1");
-              expect(isntemptyFilter(21, "myNumber")).toBe("myNumber 21");
-              expect(isntemptyFilter(["happy", "days", "testing", "rocks"], "arrays")).toBe('arrays happy,days,testing,rocks');
+          inject(function(isntEmptyFilter) {
+              expect(isntEmptyFilter("item1", "First Item")).toBe("First Item item1");
+              expect(isntEmptyFilter(21, "myNumber")).toBe("myNumber 21");
+              expect(isntEmptyFilter(["happy", "days", "testing", "rocks"], "arrays")).toBe('arrays happy,days,testing,rocks');
           }));
     });
 });
