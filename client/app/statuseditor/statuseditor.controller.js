d6ff14b4464cea1014b198bf6aee7cde669050f3
ricc0082 <ricc0082@umn.edu>
2014-11-17 14:35:08 -0600

    StatusEditor submit button is now pushing changes

--- client/app/statuseditor/statuseditor.controller.js
+++ client/app/statuseditor/statuseditor.controller.js
@@ -24,18 +24,18 @@ angular.module('umm3601ursamajorApp')
         $scope.submitChanges = function() {
             var r = confirm("Are you sure you want to edit the status?")
             if (r == true) {
-                $http.put('/api/statuss/' + $scope.status._id,
-                    {
-                    strict: $scope.status.strict,
-                    color: $scope.status.color,
-                    emailSubject: $scope.status.emailSubject,
-                    emailBody: $scope.status.emailBody
-                    }
-                ).success(function () {
-
-                        $location.path('/admin');
-
-                    });
-            };
+                for(var x = 0; x < $scope.statusArray.length; x++){
+                    $http.put('/api/statuss/' + $scope.statusArray[x]._id,
+                        {
+                            strict: $scope.statusArray[x].strict,
+                            color: $scope.statusArray[x].color,
+                            emailSubject: $scope.statusArray[x].emailSubject,
+                            emailBody: $scope.statusArray[x].emailBody
+                        }
+                    ).success(function () {
+                            $location.path('/admin');
+                        })
+                }
+            }
         };
-  });
+    });
