c990f335db8bafcd0bd3b2c9f6c3d3b3dae1b0ac
kklamberty <lamberty@morris.umn.edu>
2014-11-21 14:38:27 -0600

    I added the socket information here because it was complaining. I added this, and it stopped complaining. Later, when I took it out, it was still not complaining, but I put it in again anyhow.

--- client/app/subform/subform.controller.spec.js
+++ client/app/subform/subform.controller.spec.js
@@ -5,11 +5,12 @@ describe('Controller: SubformCtrl', function () {
   // load the controller's module
   beforeEach(module('umm3601ursamajorApp'));
 
-  var SubformCtrl, scope;
+  var SubformCtrl, scope, socket;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _socket_) {
     scope = $rootScope.$new();
+    socket = _socket_;
     SubformCtrl = $controller('SubformCtrl', {
       $scope: scope
     });
